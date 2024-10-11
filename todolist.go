d2481b89832e0e73206d2497066b3450aa006f04
Антон Губатых <gubatykhaa@it2g.ru>
2024-10-11 16:13:05 +0500

    add makedb script; add comment

--- todolist.go
+++ todolist.go
@@ -8,7 +8,7 @@ func main() {
 	// file web server
 	mux.Handle("/", http.FileServer(http.Dir("web")))
 
-	err := http.ListenAndServe(":7540", mux)
+	err := http.ListenAndServe(":7540", mux) // TODO: get port from env variables
 	if err != nil {
 		panic(err)
 	}
